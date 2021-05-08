a3f783c3ac1e76bb6fa8a46542fa38a60adacd37
honeygoct <hiroki.honey@gmail.com>
2021-05-08 16:05:58 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,9 @@ def index
   @posts = Post.order(id: "DESC")
 end
 
-
-
-
-
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content])
+    render json:{ post: post}
   end
 
 end
